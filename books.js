7604e435fb348d9d0bf21e292e91daaa0b9107f6
Yacoub <yacoubhaddad33@outlook.com>
2022-07-26 14:23:46 +0100

    original price & sorting(books.html)

--- books.js
+++ books.js
@@ -1,12 +1,68 @@
-
-
 // FAKE DATA
+function renderBooks(filter) {
+  const booksWrapper = document.querySelector(".books");
+
+  const books = getBooks();
+
+  if (filter === "LOW_TO_HIGH") {
+    const filteredBooks = books.sort(
+      (a, b) => a.originalPrice - b.originalPrice
+    );
+  } else if (filter === "HIGH_TO_LOW") {
+    const filteredBooks = books.sort(
+      (a, b) => b.originalPrice - a.originalPrice
+    );
+  } else if (filter === "RATING") {
+    books.sort((a, b) => b.rating - a.rating);
+  }
+
+  const booksHtml = books
+    .map((book) => {
+      return `<div class="book">
+    <figure class="book__img--wrapper">
+      <img class="book__img" src="${book.url}" alt="" />
+    </figure>
+    <div class="book__title">${book.title}</div>
+    <div class="book__ratings">
+    ${ratingsHTML(book.rating)}
+    </div>
+    <div class="book__price">
+      <span class=>£${book.originalPrice.toFixed(2)}</span>
+    </div>
+  </div>`;
+    })
+    .join("");
+
+  booksWrapper.innerHTML = booksHtml;
+}
+
+function ratingsHTML(rating) {
+  let ratingHTML = "";
+
+  for (let i = 0; i < Math.floor(rating); i++) {
+    ratingHTML += '<i class="fas fa-star"></i>';
+  }
+
+  if (!Number.isInteger(rating)) {
+    ratingHTML += '<i class="fas fa-star-half-alt"></i>';
+  }
+  return ratingHTML;
+}
+
+function filterBooks(event) {
+  renderBooks(event.target.value);
+}
+
+setTimeout(() => {
+  renderBooks();
+});
+
 function getBooks() {
   return [
     {
       id: 1,
       title: "Crack the Coding Interview",
-                url: "assets/crack the coding interview.png",
+      url: "assets/crack the coding interview.png",
       originalPrice: 49.95,
       salePrice: 14.95,
       rating: 4.5,
